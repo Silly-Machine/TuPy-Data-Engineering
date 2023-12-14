bd06ec3545d16a6ff183ea6d2c398aa8209e9d31
Victoria Reis <victoria.reis@coc.ufrj.br>
2023-12-14 17:20:39 -0300

    organize directory

--- src/figures/words_dist_plot.py
+++ src/figures/words_dist_plot.py
@@ -4,19 +4,36 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-def WordDistPlot(DataFrame, x:str, y:str, rank:int=20, palette:str='Greens_r',size:float=15,ratio:int=3)-> sns.barplot:  
-    
-    plt.figure(figsize=(size,size/ratio), dpi = 1080) 
-    #sns.set(rc={'figure.figsize': (size,size/ratio)})
-    sns.set_style("ticks")
-    
-    
-    ax = sns.barplot(x=x, y=y, palette=palette, data=DataFrame.head(rank))
-    ax.set_xlabel(x,fontsize=size*0.75,labelpad=10)
-    ax.set_ylabel(y,fontsize=size*0.75,labelpad =10)
-    ax.tick_params(labelsize=size*0.50,rotation=90)
+def WordDistPlot(DataFrame, x: str, y: str, rank: int = 20, palette: str = 'Greens_r', size: float = 15, ratio: int = 3) -> sns.barplot:
+    """
+    Generate a bar plot to visualize the distribution of words in a DataFrame.
 
-    sns.despine(bottom = False, left = False)
-    
-    ax = ax
+    Parameters:
+    - DataFrame (pd.DataFrame): The input DataFrame containing the data.
+    - x (str): The column name for the x-axis.
+    - y (str): The column name for the y-axis.
+    - rank (int, optional): The number of top entries to display. Default is 20.
+    - palette (str, optional): The color palette to use for the plot. Default is 'Greens_r'.
+    - size (float, optional): The size of the plot. Default is 15.
+    - ratio (int, optional): The aspect ratio of the plot. Default is 3.
+
+    Returns:
+    - sns.barplot: The Seaborn bar plot.
+
+    Example:
+    ```python
+    WordDistPlot(my_dataframe, 'Word', 'Frequency', rank=15, palette='Blues', size=12, ratio=2)
+    ```
+    """
+    plt.figure(figsize=(size, size / ratio), dpi=1080)
+    sns.set_style("ticks")
+
+    ax = sns.barplot(x=x, y=y, palette=palette, data=DataFrame.head(rank))
+    ax.set_xlabel(x, fontsize=size * 0.75, labelpad=10)
+    ax.set_ylabel(y, fontsize=size * 0.75, labelpad=10)
+    ax.tick_params(labelsize=size * 0.50, rotation=90)
+
+    sns.despine(bottom=False, left=False)
+
     return ax
+
--- src/text/words_counter.py
+++ src/text/words_counter.py
@@ -1,17 +1,35 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-def NgramsCount(corpus:pd.DataFrame,collum:str="text" ,nintens:int = 1, rank:int=10)-> pd.DataFrame:
-    corpus = corpus[collum].apply(str)
-    nrange = (nintens,nintens)
+def NgramsCount(corpus: pd.DataFrame, column: str = "text", ngrams: int = 1, rank: int = 10) -> pd.DataFrame:
+    """
+    Count the frequency of n-grams in a text corpus and return the top n-grams.
+
+    Parameters:
+    - corpus (pd.DataFrame): The input DataFrame containing the text data.
+    - column (str, optional): The column name containing the text data. Default is "text".
+    - ngrams (int, optional): The size of n-grams to consider. Default is 1 (unigrams).
+    - rank (int, optional): The number of top n-grams to return. Default is 10.
+
+    Returns:
+    - pd.DataFrame: A DataFrame with columns "Word"/"Bigram"/"Trigram"/"{ngrams}_gram" and "Frequency".
+
+    Example:
+    ```python
+    ngrams_df = NgramsCount(my_corpus, column='content', ngrams=2, rank=15)
+    print(ngrams_df)
+    ```
+    """
+    corpus = corpus[column].apply(str)
+    nrange = (ngrams, ngrams)
     vectorizer = CountVectorizer(ngram_range=nrange).fit(corpus)
-    ngrmas_bag = vectorizer.transform(corpus)
+    ngrams_bag = vectorizer.transform(corpus)
     words_freq = [
-        (word, ngrmas_bag.sum(axis=0)[0, idx])
+        (word, ngrams_bag.sum(axis=0)[0, idx])
         for word, idx in vectorizer.vocabulary_.items()
     ]
     words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
-    
+
     if nrange[0] == 1:
         order = "Word"
     elif nrange[0] == 2:
@@ -20,5 +38,5 @@ def NgramsCount(corpus:pd.DataFrame,collum:str="text" ,nintens:int = 1, rank:int
         order = "Trigram"
     else:
         order = f"{str(nrange[0])}_gram"
-        
+
     return pd.DataFrame(words_freq[:rank], columns=[order, "Frequency"])
